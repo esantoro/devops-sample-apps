f48c4799fe62703372d364382b2802b508e07176
Emanuele Santoro <manu@santoro.tk>
2024-08-17 17:45:13 +0200

    Add dev env files

---
+++ php/build-dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -f Dockerfile-dev -t docker.io/esantoro/dp-sample-php:latest-dev .
---
+++ php/run-dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it --rm -p 8080:80 docker.io/esantoro/dp-sample-php:latest-dev
