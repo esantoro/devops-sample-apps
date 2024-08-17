e9b52a1801d98f2c35a8da3479958a4db32e04cc
Emanuele Santoro <manu@santoro.tk>
2024-08-17 17:25:44 +0200

    Add Docker file and utility scripts

---
+++ golang/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t esantoro/dp-sample-go:latest .
---
+++ golang/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -x 
+
+docker run -it --rm -p 8080:8080 esantoro/dp-sample-go:latest
